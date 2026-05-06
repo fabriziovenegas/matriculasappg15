cbe88204a7146b86bdf29b281463ecaf9453df5d
fabriziovenegas <fabrizio.adrian.venegas@gmail.com>
2026-05-06 00:03:46 -0500

    Avance List

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:matriculasappg15/models/persona_model.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PersonaModel> personas = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                personas.add(
+                  PersonaModel(
+                    nombre: "Fabrizio",
+                    apellido: "Venegas",
+                    direccion: "Surinam",
+                    dni: "12345678",
+                    nacionalidad: "Peruano",
+                  ),
+                );
+              },
+              child: Text("Agregar persona"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
